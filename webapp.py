4ebad80c99f22d12e8f5f5b87a0997bb91a4ca43
VS.Ivanov <vds-ivanov@yandex.com>
2016-03-24 12:30:45 +0300

    fix error in load settings

--- webapp.py
+++ webapp.py
@@ -1,4 +1,28 @@
 from flask import Flask
+import yandexwebdav
+import os, io
+import json
+
+def generate_settings():
+	settings = {'disk': {'user': 'username','password':'password'}}
+
+	settings_file = open('load_settings.json','tw')
+	settings_file.write(json.dumps(settings))
+	settings_file.close()
+
+
+def load_settings():
+	setting_file = open('load_settings.json','tr')
+	settings = json.loads(setting_file.read())
+	setting_file.close()
+	return settings
+
+
+# если нет настроек, то генерим файл и открываем
+if not os.path.exists('load_settings.json'):
+	generate_settings()
+
+settings = load_settings()
 
 app = Flask(__name__)
 
@@ -6,5 +30,11 @@ app = Flask(__name__)
 @app.route('/index')
 def index():
 	return 'WebApp is running...'
+# проводим синхронизацию с яндекс диском
+if not os.path.exists('./disk'):
+	os.mkdir('./disk')
+
+ydisk = yandexwebdav.Config({'user':settings['disk']['user'],'password':settings['disk']['password']})
+ydisk.sync('./disk/*', '/reports/*')
 
 app.run()
